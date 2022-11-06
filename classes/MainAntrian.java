bb565783dfc448124b722bde7a9d6243daa502cc
tyararayna <116780511+tyararayna@users.noreply.github.com>
2022-11-06 13:34:41 +0700

    test main

---
+++ classes/MainAntrian.java
@@ -0,0 +1,11 @@
+public class MainAntrian {
+    private boolean ulang; 
+    private Pasien p1;
+    private jalurPendaftaran jp;
+    private Dokter dr;
+    private detailAntrian antri;
+    
+    public MainAntrian(){
+        
+    }
+}
