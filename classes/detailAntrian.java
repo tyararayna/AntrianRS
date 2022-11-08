4b31af2c473e479abdc81e69df7ecdca41640d0b
Ayuauliaa <116651504+Ayuauliaa@users.noreply.github.com>
2022-11-08 22:29:56 +0700

    merubah kode di kelas pasien

--- classes/detailAntrian.java
+++ classes/detailAntrian.java
@@ -12,14 +12,14 @@ public class detailAntrian{
         tail=baru;
     }
     public void deque(){
-        if(head==null)System.out.println("- Kosong -");
+        if(head==null)System.out.println("- Data Kosong -");
         else{
             System.out.println("Data Keluar Sesuai No ktp : "+head.id);
             head=head.next;
         }
     }
     public void view(){
-        if(head==null)System.out.println("- Kosong -");
+        if(head==null)System.out.println("- Data Kosong -");
         else{
             System.out.println("|| Id \t|| Nama \t|| Keperluan \t||");
             for(Pasien a=head; a!=null; a=a.next) a.read();
