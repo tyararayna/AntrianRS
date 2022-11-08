555b62467b88339f2377f62953d0e4cbb3ccedef
Ayuauliaa <116651504+Ayuauliaa@users.noreply.github.com>
2022-11-08 22:25:50 +0700

    mengubah kode pasien

--- classes/detailAntrian.java
+++ classes/detailAntrian.java
@@ -1,26 +1,28 @@
-public class detailAntrian {
-    private Pasien pasien;  
-    private int noAntrian;
-
-    public detailAntrian(Pasien pasien, int noAntrian) {
-        this.pasien = pasien;
-        this.noAntrian = noAntrian;
+class linked{
+    Pasien head,tail;
+    public linked(){
+        head=null;
+        tail=null;
     }
-
-    public Pasien getPasien() {
-        return pasien;
+    public void enque(){
+        Pasien baru=new Pasien();
+        baru.input();
+        if(head==null)head=baru;
+        else tail.next=baru;
+        tail=baru;
     }
-
-    public void setPasien(Pasien pasien) {
-        this.pasien = pasien;
+    public void deque(){
+        if(head==null)System.out.println("- Kosong -");
+        else{
+            System.out.println("Data Keluar Sesuai No ktp : "+head.id);
+            head=head.next;
+        }
     }
-
-    public int getNoAntrian() {
-        return noAntrian;
+    public void view(){
+        if(head==null)System.out.println("- Kosong -");
+        else{
+            System.out.println("|| Id \t|| Nama \t|| Keperluan \t||");
+            for(Pasien a=head; a!=null; a=a.next) a.read();
+        }
     }
-
-    public void setNoAntrian(int noAntrian) {
-        this.noAntrian = noAntrian;
-    }
-
 }
