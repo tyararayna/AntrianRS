32a38f38d7e1f66c98d1a7ece0d52a9adbe07038
Ayuauliaa <116651504+Ayuauliaa@users.noreply.github.com>
2022-11-08 22:24:37 +0700

    menambah kode pada kelas detailantrian

--- classes/Pasien.java
+++ classes/Pasien.java
@@ -1,26 +1,32 @@
-import java.util.Scanner ;
-
-public class Pasien {
-    private int ktp;
-    private String nama;
-    private String noantri ;
-    private int biayasementara ;
-
-    static Scanner in = new Scanner(System.in);
-    static Scanner str = new Scanner(System.in);
-
+import java.util.Scanner;
+public class Pasien{
+    int id;
+    String nama,jalurPendaftaran;
+    Pasien next;
+    static Scanner in=new Scanner(System.in);
+    static Scanner str=new Scanner(System.in);
     public void input(){
-        System.out.println();
-        System.out.println("\t\t\t\t\tMasukkan No Ktp :") ;
-        ktp = in.nextInt();
-        System.out.println("\t\t\t\t\tMasukkan Nama :") ;
-        nama = str.nextLine(); 
+        System.out.print("Masukkan No ktp : ");
+        id=in.nextInt();
+        System.out.print("Masukkan nama   : ");
+        nama=str.nextLine();
+        next=null;
     }
-
     public void read(){
-        System.out.println("\t\t\t\t|| " + ktp + "\t|| " +nama+" \t\t|| " );
-        
+        System.out.println("|| "+id+" \t|| "+nama+" \t|| "+jalurPendaftaran+" \t||");
+    }
+    public static void main(String[] args){
+        int menu=0;
+        linked que=new linked();
+        while(menu!=4){
+            System.out.print("1.Enqueue\n2.Dequeue\n3.View\n4.Exit\n : ");
+            menu=in.nextInt();
+            if(menu==1)que.enque();
+            else if(menu==2)que.deque();
+            else if(menu==3)que.view();
+            else if(menu==4)System.out.println("- keluar -");
+            else System.out.println("- Salah -");
+            System.out.println("");
+        }
     }
 }
-
-        
