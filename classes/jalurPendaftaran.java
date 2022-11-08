f6908cf2b94fb97725333ca64fa9188bfe79a760
tyararayna <116780511+tyararayna@users.noreply.github.com>
2022-11-08 23:30:16 +0700

    tambah method jenisDaftar di class jalurPendaftaran

--- classes/jalurPendaftaran.java
+++ classes/jalurPendaftaran.java
@@ -14,20 +14,7 @@ public class jalurPendaftaran extends Registrasi
         
     public jalurPendaftaran(int jenis)
     {
-        do{
-            System.out.println("Pilih Jalur Daftar : ");
-            jenis = input.nextInt();
-                
-            if (jenis == 1) {
-                System.out.println("Anda Memilih Jalur BPJS");
-            }else if (jenis == 2){
-                System.out.println("Anda Memilih Jalur Asuransi");
-            }else if (jenis == 3){
-                System.out.println("Anda Memilih Jalur Pribadi");
-            }
-            
-        }while(jenis > 3);
-            Biaya();
+        
     }
     
     public void setJenis(int j){
@@ -39,23 +26,25 @@ public class jalurPendaftaran extends Registrasi
         return jenis;
     }
     
-    //public void jenisDaftar(int jenis){
+    public void jenisDaftar(){
+        do{
+            System.out.println("=========Jalur Daftar=========");
+            System.out.println("\t\t1. BPJS");
+            System.out.println("\t\t2. Asuransi");
+            System.out.println("\t\t3. Pribadi");
+            System.out.print("Masukkan pilihan anda :");
+            jenis = input.nextInt();
+                
+            if (jenis == 1) {
+                System.out.println("Anda Memilih Jalur BPJS");
+            }else if (jenis == 2){
+                System.out.println("Anda Memilih Jalur Asuransi");
+            }else if (jenis == 3){
+                System.out.println("Anda Memilih Jalur Pribadi");
+            }
+            
+        }while(jenis > 3);
         
-    //}
-    
-    public int Biaya()
-    {
-    do{
-        
-        if(getJenis() == 1){
-            return Biaya; 
-        }else if(getJenis() == 2){
-            return Biaya += 25000;
-        }else if(getJenis() == 3){
-            return Biaya += 50000;
-        }
-    
-    }while(getJenis() > 3);
-        return 0;
     }
+    
 }
