5a680ca266d3fcdf80c445b59338495a51aa3a59
tyararayna <116780511+tyararayna@users.noreply.github.com>
2022-11-08 23:40:39 +0700

    tambah lagi main nya tapi blm siap siap

--- classes/MainAntrian.java
+++ classes/MainAntrian.java
@@ -20,11 +20,31 @@ public class MainAntrian {
         //System.out.println(jp.Biaya());
     }
     
-    public static void main(String[] args) {
-        MainAntrian main = new MainAntrian();
-    
-        main.mulai();
-
+    public static void main(String[] args){
+        MainAntrian m = new MainAntrian();
+        int menu = 0;
+        m.mulai();
         
+        detailAntrian que=new detailAntrian();
+        do{
+            System.out.print("1.Enqueue\n2.Dequeue\n3.View\n4.Exit\n : ");
+            menu = in.nextInt();
+            
+            if(menu==1){
+                que.enque();   
+            }
+            else if(menu==2){
+                que.deque();
+            }
+            else if(menu==3){
+                que.view();
+            }else if(menu==4){
+                System.out.println("- keluar -");
+            }else {
+                System.out.println("- Salah -");
+                System.out.println("");
+            }
+            m.mulai();
+        }while(menu != 4);
     }
 }
