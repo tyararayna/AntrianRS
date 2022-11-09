b2b6c29ed058c83b06b37fc9baeeaac35347e3c5
tyararayna <116780511+tyararayna@users.noreply.github.com>
2022-11-09 19:37:46 +0700

    tambah java doc

--- classes/Registrasi.java
+++ classes/Registrasi.java
@@ -9,10 +9,18 @@ public abstract class Registrasi{
     private int Biaya;
     private List<detailAntrian> Antrian = new ArrayList<detailAntrian>() ;
 
+    /**
+     * Method Constructor untuk menginisialisasikan biaya
+     */
     public Registrasi(){
         Biaya = 25000;
     }
     
+    /**
+     * Method Accessor untuk mengembalikan biaya
+     * @param b
+     * @return Biaya
+     */
     public int Biaya(int b){
         return Biaya;
     }
