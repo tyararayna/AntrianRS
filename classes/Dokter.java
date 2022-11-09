c9fcfdc56438a09424449730240497cd3423a009
tyararayna <116780511+tyararayna@users.noreply.github.com>
2022-11-09 11:21:23 +0700

    ubah class main, dokter

--- classes/Dokter.java
+++ classes/Dokter.java
@@ -1,75 +1,26 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.io.IOException;
 
 public class Dokter {
     public Scanner input = new Scanner(System.in);
     private String nama;
-    private String poli;
-    private int Ruang;
+    //private String[] poli;
+    //private int[] Ruang;
     private int pilihan;
-    private Pasien p1;
+    //private Pasien ps;
     
-        public Dokter(String nama, int Ruang) {
-            this.nama = nama; 
-            this.Ruang = Ruang;
+        public Dokter(String nama) {
+            
         }
     
-        public String getNama() throws IOException{
-        // try{
-        //     File file = new File("DataDokter.txt");
-            
-        //     Scanner scan = new Scanner(file);
-        //     while(scan.hasNextLine()){
-        //         String getDataString = scan.nextLine();
-        //         System.out.println(getDataString);
-        //     }
-        //     scan.close();
-        // }catch(FileNotFoundException ex){
-        //     System.out.println("File Tidak Ditemukan"); 
-        // }
+        public String getNama(){
             return nama;
         }
-    
-        public void setNama(String nama) {
+        
+        public void setNama(){
             this.nama = nama;
         }
-    
-        public String getPoli(){
-            return poli;
-        }
-
-        public void setPoli(String poli){
-            this.poli = poli; 
-        }
-        public int getRuang() {
-            return Ruang;
-        }
-    
-        public void setRuang(int Ruang) {
-            this.Ruang = Ruang;
-        }
-
-        public void dataPoli(){
-            System.out.println("\tPilih jenis poli : ");
-            System.out.println("\t\t\t 1. Poli Umum");
-            System.out.println("\t\t\t 2. Poli Kulit");
-            System.out.println("\t\t\t 3. Poli Mata");
-            System.out.println("\t\t\t 4. Poli THT");
-            System.out.println("\t\t\t 5. Poli Gigi");
-            System.out.println("\t\t\t 6. Poli Kandungan");
-
-            pilihan = input.nextInt();
-            switch(pilihan) {
-                case '1' :
-                System.out.println(p1.getNama() + " dengan Dokter  : dr. Hayatun Nufus" + "Ruang : 1" + p1.getnoantri());
-                break;
-                
-                case '2' : 
-                System.out.println(p1.getNama() + " dengan Dokter  : dr. Hayatun Nufus" + "Ruang : 1" + p1.getnoantri());
-                break;
-            }
-        }
         
-    }
+        public void display(){
+            System.out.print(nama);
+        }
+}
---
+++ classes/main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+/**
+ * Write a description of class main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class main
+{
+    static Scanner in = new Scanner(System.in);
+    private jalurPendaftaran jp;
+    private Dokter dr;
+    private Pasien ps;
+    private int pilihan;
+    
+    public void mulai(){
+        ps = new Pasien();
+        //ps.printNama();
+        
+        jp = new jalurPendaftaran(1);
+        jp.jenisDaftar();
+        
+        //jp.Biaya();
+        //System.out.println(jp.Biaya());
+    }
+    
+    public static void main(String[] args){
+        main m = new main();
+        int menu=0;
+        m.mulai();
+        
+        Dokter[] arr;
+        
+        arr = new Dokter[6];
+        arr[0] = new Dokter("Ayu");
+        int pilihan = 0;
+        
+        System.out.println("\tPilih jenis poli : ");
+            System.out.println("\t\t\t 1. Poli Umum");
+            System.out.println("\t\t\t 2. Poli Kulit");
+            System.out.println("\t\t\t 3. Poli Mata");
+            System.out.println("\t\t\t 4. Poli THT");
+            System.out.println("\t\t\t 5. Poli Gigi");
+            System.out.println("\t\t\t 6. Poli Kandungan\n");
+            System.out.print("\t\t\t Masukkan Pilihan Anda : ");
+        
+            pilihan = in.nextInt();
+            
+            if(pilihan == 1){
+                System.out.print(" dengan Dokter :" ); 
+                arr[0].display();
+            }
+            
+        detailAntrian que=new detailAntrian();
+        do{
+            System.out.print("\n1.Enqueue\n2.Dequeue\n3.View\n4.Exit\n : ");
+            menu = in.nextInt();
+            
+            if(menu==1){
+                que.enque();   
+            }
+            else if(menu==2){
+                que.deque();
+            }
+            else if(menu==3){
+                que.view();
+            }else if(menu==4){
+                System.out.println("- keluar -");
+            }else {
+                System.out.println("- Salah -");
+                System.out.println("");
+            }
+            
+            m.mulai();
+            
+        }while(menu != 4);
+    }
+
+}    
